f18224c9aa356346d79225ea56d87b020ae6a2fb
JIM2022 <jaymail04@gmail.com>
2017-08-13 12:16:12 +0200

    1. Fonction Edit "Destinations" = Ok 2. Fonction Delete "Destinations" = Ok
    	modified:   .sass-cache/f8a1c1dc656d666b1486290cefd1ad4cc2d3a8af/_photo.scssc
    	modified:   app/Resources/views/base.html.twig
    	modified:   src/SeoBundle/Controller/MediaController.php
    	modified:   src/SeoBundle/Resources/config/routing.yml#
    	modified:   src/SeoBundle/Resources/public/styles/sass/_photo.scss
    	modified:   src/SeoBundle/Resources/public/styles/stylesheets/main.css
    	modified:   src/SeoBundle/Resources/views/Default/add.html.twig
    	new file:   src/SeoBundle/Resources/views/Default/edit_destination.html.twig
    	modified:   src/SeoBundle/Resources/views/Default/photos.html.twig

--- src/SeoBundle/Controller/MediaController.php
+++ src/SeoBundle/Controller/MediaController.php
@@ -3,50 +3,22 @@
 namespace SeoBundle\Controller;
 
 use SeoBundle\Entity\Article;
-use SeoBundle\Entity\Media;
 use SeoBundle\Form\ArticleType;
-use SeoBundle\Form\MediaType;
-use SeoBundle\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use SeoBundle\Form\MediaType;
+use SeoBundle\Entity\Media;
+use SeoBundle\Repository\ArticleRepository;
 
 class MediaController extends Controller
 {
-    public function listAction()
-    {
-        $em = $this ->getDoctrine()->getManager();
-        $article = $em ->getRepository(Article::class)->findAll();
 
-        return $this->render('@Seo/Default/photos.html.twig', array(
-            'articles'=>$article
-        ));
-
-    }
-
-
-    public function addAction(Request $request)
-    {
-        $article = new Article();
-
-        $form =$this->createForm(ArticleType::class, $article);
-
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid())
-        {
-            $em=$this->getDoctrine()->getManager();
-            $em->persist($article);
-            $em->flush();
-
-            return $this->redirectToRoute('add');
-        }
-        return $this->render('@Seo/Default/photos.html.twig', array (
-            'form' => $form->createView()
-
-        ));
-
-    }
-
-    public function pictureFormAction(Request $request)
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * List all Destinations & add Destinations
+     */
+    public function destinationFormAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $articles = $em->getRepository(Article::class)->findAll();
@@ -63,7 +35,6 @@ class MediaController extends Controller
 
             return $this->redirectToRoute('photo');
         }
-
         return $this->render('@Seo/Default/photos.html.twig', array(
             'articles' => $articles,
             'form' => $form->createView()
@@ -71,6 +42,44 @@ class MediaController extends Controller
         ));
     }
 
+    public function destinationEditAction(Request $request, Article $article)
+    {
+        /*$deleteForm = $this->createDeleteForm($article);*/
+        $editForm = $this->createForm('SeoBundle\Form\ArticleType', $article);
+        $editForm->handleRequest($request);
 
+        if ($editForm->isSubmitted() && $editForm->isValid())
+        {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('photo', array('id' => $article->getId()));
+        }
+
+        return $this->render('@Seo/Default/edit_destination.html.twig', array(
+            'articles' => $article,
+            'form' => $editForm->createView(),
+            /*'delete_form' => $deleteForm->createView(),*/
+        ));
+    }
+
+    public function destinationDeleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $article = $em->getRepository(Article::class)->findOneById($id);
+        $em->remove($article);
+        $em->flush();
+
+        return $this->redirectToRoute('photo');
+    }
+
+    /*public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $activite = $em->getRepository('CoreBundle:Activite')->findOneById($id);
+        $em->remove($activite);
+        $em->flush();
+
+        return $this->redirectToRoute('core_activite_add');
+    }*/
 }
 
